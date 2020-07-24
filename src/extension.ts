66b44625cdbde3d705755fd744d9ad530cae50ac
lin_bo <710850609@qq.com>
2020-07-24 18:37:36 +0800

    0.3.3

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,9 @@ import * as vscode from 'vscode';
 import { AutoJsDebugServer, Device } from './autojs-debug';
 import * as oldAutojs from './autojs-debug-old';
 import { ProjectTemplate, Project } from './project';
-import { join } from 'path';
+import * as path from 'path';
+import * as fs from 'fs'
+import { Context } from 'vm';
 
 var server = new AutoJsDebugServer(9317);
 var oldServer = new oldAutojs.AutoJsDebugServer(1209);
@@ -47,6 +49,92 @@ oldServer
 
 
 class Extension {
+
+    private documentViewPanel: any = undefined;
+
+    openDocument() {
+        console.log('准备打开文档');
+        if (this.documentViewPanel) {
+            this.documentViewPanel.reveal(vscode.ViewColumn.Beside);
+        } else {
+            // 1.创建并显示Webview
+            this.documentViewPanel = (vscode.window as any).createWebviewPanel(
+                // 该webview的标识，任意字符串
+                'Auto.js Document',
+                // webview面板的标题，会展示给用户
+                'Auto.js开发文档',
+                // webview面板所在的分栏
+                (vscode.ViewColumn as any).Beside,
+                // 其它webview选项
+                {
+                    // Enable scripts in the webview
+                    enableScripts: true
+                }
+            );
+            // Handle messages from the webview
+            this.documentViewPanel.webview.onDidReceiveMessage(message => {
+                console.log('插件收到的消息：' + message.href);
+                let href = message.href.substring(message.href.indexOf("\/electron-browser\/") + 18);
+                console.log("得到uri：" + href)
+                this.loadDocument(href)
+            }, undefined, _context.subscriptions);
+            this.documentViewPanel.onDidDispose(() => {
+                    this.documentViewPanel = undefined;
+                },
+                undefined,
+                _context.subscriptions
+            );
+        }
+        try {
+            // 默认加载首页
+            this.loadDocument("index.html");
+        } catch (e) {
+            console.trace(e)
+        }
+    }
+
+    private loadDocument(fileName) {
+        try {
+            let docRootPath = path.join(_context.extensionPath, "src", "document");
+            let resourcePath = path.resolve(docRootPath, fileName);
+            console.log(resourcePath)
+            // let anchorIndex = resourcePath.lastIndexOf("#");
+            // let pagePath = resourcePath.substring(0, anchorIndex);
+            // let anchor = resourcePath.substring(anchorIndex);
+            let html = fs.readFileSync(resourcePath, 'utf-8');
+            // // vscode不支持直接加载本地资源，需要替换成其专有路径格式，这里只是简单的将样式和JS的路径替换
+            html = html.replace(/(<link.+?href="|<script.+?src="|<img.+?src=")(.+?)"/g, (m, $1, $2) => {
+                console.log($2.substring($2.length - 4, $2.length))
+                if ($2.substring($2.length - 4, $2.length) != 'html') {
+                    console.log("处理html：" + $1 + vscode.Uri.file(path.resolve(docRootPath, $2)).with({ scheme: 'vscode-resource' }).toString() + '"')
+                    return $1 + vscode.Uri.file(path.resolve(docRootPath, $2)).with({ scheme: 'vscode-resource' }).toString() + '"';
+                } else {
+                    console.log("处理非html:" + $1 + $2)
+                    return $1 + $2 + '"';
+                }
+            });
+            // console.log(html)
+            this.documentViewPanel.webview.html = html + 
+            `<script>
+                const vscode = acquireVsCodeApi();
+                document.querySelectorAll("a").forEach(e => {
+                    if (e) {
+                        e.onclick = () =>{
+                            if (e.href) {
+                                if (location.href.indexOf(e.href) < 0) {
+                                    vscode.postMessage({href: e.href});
+                                } else {
+                                    console.log("内部跳转：" + e.href)
+                                }
+                            }
+                        }
+                    }
+                })
+            </script>`;
+        } catch (e) {
+            console.trace(e);
+        }
+    }
 
     startServer() {
         server.listen();
@@ -209,15 +297,17 @@ class Extension {
 };
 
 
-const commands = ['startServer', 'stopServer', 'run', 'runOnDevice', 'stop', 'stopAll', 'rerun', 'save', 'saveToDevice', 'newProject',
+let _context: any;
+const commands = ['openDocument', 'startServer', 'stopServer', 'run', 'runOnDevice', 'stop', 'stopAll', 'rerun', 'save', 'saveToDevice', 'newProject',
             'runProject', 'saveProject'];
 let extension = new Extension();
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('extension "auto-js-vscodeext" is now active.');
+    console.log('extension "auto-js-vscodeext-fixed" is now active.');
     commands.forEach((command) => {
         let action: Function = extension[command];
         context.subscriptions.push(vscode.commands.registerCommand('extension.' + command, action.bind(extension)));
+        _context = context;
     })
 }
 
